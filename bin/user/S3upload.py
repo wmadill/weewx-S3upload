0aeb40b347eb6f8c00e8b2b182f739143a3b0938
Bill Madill <bill@jamimi.com>
2024-10-10 19:52:06 -0700

    Update message formats; move s3cmd output parsing to function

--- bin/user/S3upload.py
+++ bin/user/S3upload.py
@@ -39,50 +39,48 @@ import configobj
 from weeutil.weeutil import timestamp_to_string, option_as_list
 import weewx
 
+MSG_BASE = "s3uploadgenerator: "
+
 # Inherit from the base class ReportGenerator
 class S3uploadGenerator(weewx.reportengine.ReportGenerator):
     """Custom service to upload files to an S3 bucket"""
 
     # Set up logging for both weewx 3 and weewx 4 from tkeffer's blog
     # post.
-    
-    # This syntax is really dorky--there must be a better way without
-    # having all the "self." on names. But this works as offensive
-    # as it is....
     try:
         # Test for new-style weewx logging by trying to import weeutil.logger
         import weeutil.logger
         import logging
         log = logging.getLogger(__name__)
-    
+
         def logdbg(self, msg):
-            self.log.debug(msg)
-    
+            self.log.debug(MSG_BASE + msg)
+        
         def loginf(self, msg):
-            self.log.info(msg)
-    
+            self.log.info(MSG_BASE + msg)
+        
         def logerr(self, msg):
-            self.log.error(msg)
-    
+            self.log.error(MSG_BASE + msg)
+        
     except ImportError:
         # Old-style weewx logging
         import syslog
-    
+
         def logmsg(self, level, msg):
-            self.syslog.syslog(level, 's3uploadgenerator: %s:' % msg)
-    
+            self.syslog.syslog(level, MSG_BASE + "%s:" % msg)
+        
         def logdbg(self, msg):
             self.logmsg(self.syslog.LOG_DEBUG, msg)
-    
+
         def loginf(self, msg):
             self.logmsg(self.syslog.LOG_INFO, msg)
-    
+
         def logerr(self, msg):
             self.logmsg(self.syslog.LOG_ERR, msg)
 
     def run(self):
-        self.logdbg("""s3uploadgenerator: start S3uploadGenerator""")
-        self.logdbg("s3uploadgenerator: python version: "  + sys.version)
+        self.logdbg("start S3uploadGenerator")
+        self.logdbg("python version: "  + sys.version)
 
         # Get the options from the configuration dictionary and credential file.
         # Raise an exception if a required option is missing.
@@ -91,7 +89,7 @@ class S3uploadGenerator(weewx.reportengine.ReportGenerator):
             self.local_root = os.path.join(self.config_dict['WEEWX_ROOT'], html_root) + "/"
             self.bucket_name = self.skin_dict['bucket_name']
 
-            self.logdbg("s3uploadgenerator: upload configured from '%s' to '%s'" % (self.local_root, self.bucket_name)) 
+        self.logdbg("upload configured from '%s' to '%s'" % (self.local_root, self.bucket_name)) 
             
         except KeyError as e:
             self.loginf("s3uploadgenerator: no upload configured. %s" % e)
@@ -105,18 +103,18 @@ class S3uploadGenerator(weewx.reportengine.ReportGenerator):
             self.loginf("s3uploadgenerator: 's3cmd' cannot be found")
             exit(1)
 
-        self.logdbg("s3uploadgenerator: s3cmd location: "  + self.s3cmd_path)
-        self.logdbg("s3uploadgenerator: uploading")
+        self.logdbg("s3cmd location: "  + self.s3cmd_path)
+        self.logdbg("uploading")
 
         # Launch in a separate thread so it doesn't block the main LOOP thread:
         t  = threading.Thread(target=self.uploadFiles)
         t.start()
-        self.logdbg("s3uploadgenerator: return from upload thread")
 
+    # The thread that does all the work
     def uploadFiles(self):
         start_ts = time.time()
         t_str = timestamp_to_string(start_ts)
-        self.logdbg("s3uploadgenerator: start upload at %s" % t_str)
+        self.logdbg("start upload at %s" % t_str)
 
         # Build s3cmd command string
         cmd = [self.s3cmd_path]
@@ -130,58 +128,66 @@ class S3uploadGenerator(weewx.reportengine.ReportGenerator):
         cmd.extend(["--guess-mime-type"])
         cmd.extend(["--no-mime-magic"])
 
-        self.logdbg("s3uploadgenerator: command: %s" % cmd)
+        self.logdbg("command: %s" % cmd)
         try:
             S3upload_cmd = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             stdout = S3upload_cmd.communicate()[0]
             stroutput = stdout.strip()
         except OSError as e:
             if e.errno == errno.ENOENT:
-                self.logerr("s3uploadgenerator: s3cmd does not appear to be installed on this system. (errno %d, \"%s\")" % (e.errno, e.strerror))
+                self.logerr("s3cmd not installed at %s. (errno %d, \"%s\")" % (self.s3cmd_path, e.errno, e.strerror))
             raise
         
-        if weewx.debug == 1:
-            self.logdbg("s3uploadgenerator: s3cmd output: %s" % stroutput)
-            for line in iter(stroutput.splitlines()):
-                self.logdbg("s3uploadgenerator: s3cmd output: %s" % line)
-
-        # S3upload output. generate an appropriate message
-        if stroutput.find(b'Done. Uploaded ') >= 0:
-            file_cnt = 0
-            for line in iter(stroutput.splitlines()):
-                # Not sure what a specific upload failure looks like.
-                # This is what s3cmd version 1.6.1 returns on successful upload.
-                # Note that this is from the Debian repos and is ooolllldddd
-                if line.find(b'upload: ') >= 0:
-                    file_cnt += 1
-                if line.find(b'Done. Uploaded ') >= 0:
-                    # get number of bytes uploaded
-                    m = re.search(r"Uploaded (\d*) bytes", str(line))
-                    if m:
-                        byte_cnt = int(m.group(1))
-                    else:
-                        byte_cnt = "Unknown"
-
-            # format message
-            try:
-                if file_cnt is not None and byte_cnt is not None:
-                    S3upload_message = "uploaded %d files (%s bytes) in %%0.2f seconds" % (int(file_cnt), byte_cnt)
-                else:
-                    S3upload_message = "executed in %0.2f seconds"
-            except:
-                S3upload_message = "executed in %0.2f seconds"
-        else:
-            # suspect we have an s3cmd error so display a message
-            self.loginf("s3uploadgenerator: s3cmd reported errors")
-            for line in iter(stroutput.splitlines()):
-                self.loginf("s3uploadgenerator: s3cmd error: %s" % line)
-            S3upload_message = "executed in %0.2f seconds"
-        
         stop_ts = time.time()
-        self.loginf("s3uploadgenerator: results: "  + S3upload_message % (stop_ts - start_ts))
+        time_msg = "executed in %0.2f seconds" % (stop_ts - start_ts)
+        upload_msg = self.parseoutput(stroutput)
+        if upload_msg == "":
+            self.loginf(time_msg)
+        else:
+            self.loginf(upload_msg + "; " + time_msg)
 
         t_str = timestamp_to_string(stop_ts)
-        self.logdbg("s3uploadgenerator: end upload at %s" % t_str)
+        self.logdbg("end upload at %s" % t_str)
+    # Parse s3cmd output, generate, and return an appropriate message.
+    # This output parsing routine is based on what s3cmd 1.6.1 returned
+    # with various tests. While the output returned from a successful
+    # upload is tested, I could not provoke any upload errors so
+    # they are just returned as is.
+    # Also tested with s3cmd version 2.4.
+    def parseoutput(self, stroutput):
+        self.logdbg("s3cmd full output: %s" % stroutput)
+
+        # If only an empty string returned, there was nothing to upload
+        if stroutput == b'':
+            return "no changes to upload"
+
+        line_num = 0
+        for line in iter(stroutput.splitlines()):
+            line_num += 1
+            self.logdbg("s3cmd line %s: %s" % (line_num, line))
+
+        if stroutput.find(b'Done. Uploaded ') < 0:
+            # suspect we have an s3cmd error so display a message
+            self.loginf("s3cmd reported errors")
+            line_num = 0
+            for line in iter(stroutput.splitlines()):
+                line_num += 1
+                self.loginf("s3cmd line %s: %s" % (line_num, line))
+            return ""
+
+        file_cnt = 0
+        for line in iter(stroutput.splitlines()):
+            if line.find(b'upload: ') >= 0:
+                file_cnt += 1
+            if line.find(b'Done. Uploaded ') >= 0:
+                # get number of bytes uploaded
+                m = re.search(r"Uploaded (\d*) bytes", str(line))
+                if m:
+                    byte_msg = "(%d bytes)" % int(m.group(1))
+                else:
+                    byte_msg = "Unknown number of bytes"
+
+        return "uploaded %d files " % file_cnt + byte_msg
 
 if __name__ == '__main__':
     """This section is used for testing the code. """
